d38a26499ac3ed4848651f92ef864e444df4758b
Karina <milkokarina13@gmail.com>
2024-08-22 23:54:47 +0300

    Add a component from the atom site

--- practice-project-after-front (copy)/client/src/pages/Home/SecondNamingPlatform/SecondNamingPlatform.jsx
+++ practice-project-after-front (copy)/client/src/pages/Home/SecondNamingPlatform/SecondNamingPlatform.jsx
@@ -1,26 +1,34 @@
 import { Link } from 'react-router-dom';
 import data from './data';
+import styles from './SecondNamingPlatform.module.sass';
 
 function SecondNamingPlatform() {
-  const mapItems = ({ icon, title, body }, i) => {
-    <li key={i}>
-      <img src={icon} alt="icon" />
-      <h3>{title}</h3>
-      <p>{body}</p>
-    </li>;
-  };
+  const mapItems = ({ icon, title, body }, i) => (
+    <li key={i} className={styles.listItemsSecondNamingPlatform}>
+      <img className={styles.iconSecondNamingPlatform} src={icon} alt="icon" />
+      <h3 className={styles.iconHeaderSecondNamingPlatform}>{title}</h3>
+      <p className={styles.iconTextSecondNamingPlatform}>{body}</p>
+    </li>
+  );
 
   return (
-    <section>
-      <h2>Like A Naming Agency, But Better</h2>
-      <p>
+    <section className={styles.containerSecondNamingPlatform}>
+      <h2 className={styles.headerSecondNamingPlatform}>
+        Like A Naming Agency, But Better
+      </h2>
+      <p className={styles.textSecondNamingPlatform}>
         With Atom's managed contests, leverage our team's naming expertise and
         our pool of 300K+ naming experts. Our hybrid-solution partners you with
         a trained Atom branding consultant who will guide your crowdsourcing
         process step-by-step to get the best results possible.
       </p>
-      <ul>{data.map(mapItems)}</ul>
-      <Link to="#">Learn More</Link>
+      <ul className={styles.listSecondNamingPlatform}>{data.map(mapItems)}</ul>
+      <div className={styles.btnSecondNamingPlatform}>
+        {' '}
+        <Link to="#">
+          <span className={styles.textBtn}>Learn More</span>{' '}
+        </Link>
+      </div>
     </section>
   );
 }
